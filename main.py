bfa0b87e7de028e5f70705361d64341fe3a1c22f
Khabir <hakim1khabir2@gmail.com>
2022-11-18 20:15:48 +0100

    Starting Point
    Create main.py, Add Basic App Structure, Add Basic App Controls

---
+++ main.py
@@ -0,0 +1,10 @@
+import flet
+from flet.ref import Ref
+from flet import Page, Text, ElevatedButton, TextField, Dropdown, dropdown, Row, KeyboardEvent
+
+
+def main(page: Page):
+    ...
+
+if __name__ == "__main__":
+    flet.app(target=main)
